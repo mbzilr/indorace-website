2021f11ea87672f14bde9104a6843131c7a8e5fa
mbzilr <asterion911ts@gmail.com>
2025-03-03 10:56:25 +0700

    Initial Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
+
+import svelte from "@astrojs/svelte";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  integrations: [react(), svelte(), vue(),
+  ]
+});
